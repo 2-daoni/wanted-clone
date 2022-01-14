de73e68c00e5b85e2c019730c5cdc6fa3f2e16a9
dawon <lyumilove9@gmail.com>
2022-01-14 21:58:14 +0900

    css 수정중

--- src/GNB.js
+++ src/GNB.js
@@ -26,12 +26,14 @@ const GNB = () => {
           </li>
           <li>
             <a href="#">커뮤니티</a>
+            <p className="new">New</p>
           </li>
           <li>
             <a href="#">프리랜서</a>
           </li>
           <li>
             <a href="#">AI합격예측</a>
+            <p className="new">New</p>
           </li>
         </ul>
         <ul className="etc">
@@ -71,13 +73,7 @@ const GNB = () => {
                   </g>
                 </g>
               </svg>
-            </a>
-          </li>
-          <li>
-            <a href="#" className="profileBox">
-              <img src={process.env.PUBLIC_URL + 'images/profile_default.png'} className="profile"></img>
-
-              <div className="profileN">
+              <span className="bellN">
                 <svg className="" width="5" height="5" viewBox="0 0 6 6">
                   <g fill="#fff" fillRule="nonzero">
                     <path
@@ -86,8 +82,25 @@ const GNB = () => {
                     ></path>
                   </g>
                 </svg>
-              </div>
+              </span>
             </a>
+          </li>
+          <li>
+            <div className="profileC">
+              <a href="#" className="profileBox">
+                <img src={process.env.PUBLIC_URL + 'images/profile_default.png'} className="profile"></img>
+                <span className="profileN">
+                  <svg className="" width="5" height="5" viewBox="0 0 6 6">
+                    <g fill="#fff" fillRule="nonzero">
+                      <path
+                        d="M6.647 11L6.647 7.259 6.688 7.259 9.158 11 11 11 11 5 9.353 5 9.353 8.357 9.322 8.357 7.089 5 5 5 5 11z"
+                        transform="translate(-123 -375) translate(20 365) translate(98 5)"
+                      ></path>
+                    </g>
+                  </svg>
+                </span>
+              </a>
+            </div>
           </li>
         </ul>
         <button className="serviceBtn">
